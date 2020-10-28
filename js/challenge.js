60342c1fd48c554a04f20ce64dc2304786e76791
skanagui <s.kanagui@gmail.com>
2020-10-28 15:55:39 -0400

    nearly done

--- js/challenge.js
+++ js/challenge.js
@@ -1,10 +1,8 @@
 
-// As a user, I can pause the counter, which should
-// pause the counter
-// disable all buttons except the pause button
 // the pause button should then show the text "resume."
 
-// When 'resume' is clicked, it should restart the counter and re-enable the buttons. 5. As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
+// When 'resume' is clicked, it should restart the counter and re-enable the buttons. 
+//5. As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
 
 // Hint for the timer
 // If you're not sure how to create or pause a timer, look into: setTimeout setInterval clearinterval
@@ -15,7 +13,7 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
     
-    setInterval(function(){
+    let timer = setInterval(function(){
         const counter = document.querySelector('#counter')
         const currentTime = parseInt(counter.textContent)
         counter.textContent = currentTime + 1
@@ -24,10 +22,10 @@ document.addEventListener("DOMContentLoaded", function(e){
     document.addEventListener("click", function(e){
         const counter = document.querySelector('#counter')
         const currentTime = parseInt(counter.textContent)
-
+        
         function likeCounter(){
             //know what current time it is
-
+            
             //find the DOM ul object
             const ulParent = document.querySelector('ul.likes')
             //add an li associated with that current time
@@ -38,19 +36,19 @@ document.addEventListener("DOMContentLoaded", function(e){
             likesLi.innerHTML = `${currentTime} was liked <span>${likes}</span> times!`
             // debugger
             if (ulParent.querySelector(`.${likesLi.className}`)) {
-                console.log("its already there")
+                
                 // debugger
                 let likedNum = parseInt(ulParent.querySelector(`.${likesLi.className}`).querySelector('span').textContent)
                 ulParent.querySelector(`.${likesLi.className}`).querySelector('span').textContent = likedNum + 1
                 
             } else {
                 ulParent.append(likesLi)
-                console.log("it's not there")
+                
             }
             // likesLi.textContent 
             // if this current time was already liked then increment to THAT li if not create a new li
             //`${currentTime} was liked {currentlikes}`
-                //li has a attribute associated with it's current time
+            //li has a attribute associated with it's current time
             //append li object to ul object parent
             // ulParent.append(likesLi)
         }
@@ -62,9 +60,55 @@ document.addEventListener("DOMContentLoaded", function(e){
         } else if (e.target.id === 'heart'){
             likeCounter()
         } else if (e.target.id === 'pause'){
+            clearInterval(timer)
+            
+            document.querySelector('#minus').disabled = true
+            document.querySelector('#plus').disabled = true
+            document.querySelector('#heart').disabled = true
+            document.querySelector('#submit').disabled = true
+
+            e.target.textContent = 'resume'
+            e.target.id = 'resume'
+            
+            
+            
+        }else if (e.target.id === 'resume'){
+            let timer = setTimeout(function(){
+                const counter = document.querySelector('#counter')
+                const currentTime = parseInt(counter.textContent)
+                counter.textContent = currentTime + 1
+                
+            }, 1000)
+
+            document.querySelector('#minus').disabled = false
+            document.querySelector('#plus').disabled = false
+            document.querySelector('#heart').disabled = false
+            document.querySelector('#submit').disabled = false
+
+            e.target.textContent = 'pause'
+            e.target.id = 'pause'
             
         }
+
+
     })
+
+    document.addEventListener('submit', function(e){
+        e.preventDefault()
+        const form = e.target
+        const comment = form.comment.value 
+        const commentP = document.createElement('p')
+        commentP.textContent = comment
+        document.querySelector('#list').append(commentP)
+        form.reset()
+
+
+
+    })
+    
+    
+    
+    
     
     
 })
@@ -76,3 +120,6 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 // As a user, I should see the timer increment every second once the page has loaded.
 // As a user, I can manually increment and decrement the counter using the plus and minus buttons.
+// As a user, I can pause the counter, which should
+// pause the counter
+// disable all buttons except the pause button
